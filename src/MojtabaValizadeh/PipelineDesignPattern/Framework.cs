186d28fdb14efb2aa01c80c0c7b35c3cb6780bf7
Valizadeh <valizadeh@edsab.com>
2023-12-18 21:32:41 +0330

    add pipline design pattern project

---
+++ src/MojtabaValizadeh/PipelineDesignPattern/Framework.cs
@@ -0,0 +1,28 @@
+﻿using Dumpify;
+
+namespace PipelineDesignPattern;
+
+public class Framework
+{
+    public delegate void Actions<T>(HttpContext httpContext);
+    public void Authentication(Actions<HttpContext> func,HttpContext httpContent)
+    {
+        if(httpContent.Ip=="192.168.0.130")
+        {
+            throw new Exception("you are from iran");
+        }
+        func(httpContent);
+    }
+
+    public void ExceptionHandling(Actions<HttpContext> func, HttpContext httpContent)
+    {
+        try
+        {
+            func(httpContent);
+        }
+        catch (Exception e)
+        {
+            "catched".Dump(e.Message);
+        }
+    }
+}
---
+++ src/MojtabaValizadeh/PipelineDesignPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+using PipelineDesignPattern;
+
+Framework framework = new();
+HttpContext iran = new HttpContext { Ip = "192.168.0.130" };
+HttpContext america = new HttpContext { Ip = "192.168.0.131" };
+CountryController countryController = new();
+framework.ExceptionHandling((context)=>framework.Authentication(countryController.GetCountryByID,context),iran);
+framework.ExceptionHandling((context)=>framework.Authentication(countryController.GetCountryByID,context),america);
+
+public class HttpContext
+{
+    public string Ip { get; set; }
+}
