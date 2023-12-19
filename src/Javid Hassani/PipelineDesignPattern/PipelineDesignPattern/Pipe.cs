a537cb66f58457e2a51a2ce7e1e0a4fe4c2b973f
javidleo <javidleo.ef@gmail.com>
2023-12-19 20:32:37 +0330

    adding sample pipeline

---
+++ src/Javid Hassani/PipelineDesignPattern/PipelineDesignPattern/Pipe.cs
@@ -0,0 +1,48 @@
+﻿using Dumpify;
+using PipelineDesignPattern.Exceptions;
+
+namespace PipelineDesignPattern;
+
+public class Pipe
+{
+    public void ExceptionHandling(Context context, Action<Context> action)
+    {
+        try
+        {
+            "ExceptionHandling Started".Dump();
+
+            action(context);
+
+            "ExceptionHandling End".Dump();
+        }
+        catch (Exception ex)
+        {
+            int statusCode = ex switch
+            {
+                ForbiddenAccessException => 406,
+                Exception => 500
+            };
+
+            $"Error {statusCode} : {ex.Message}".Dump();
+        }
+    }
+
+    public void Authurization(Context context, Action<Context> action)
+    {
+        "Authorization Started".Dump();
+        if (context.Ip.StartsWith("192.168"))
+            throw new ForbiddenAccessException("Sorry You're Not Allowed to use this service");
+
+        action(context);
+        "Authorization End".Dump();
+    }
+
+    public void Cors(Context context, Action<Context> action)
+    {
+        "Start CORS".Dump();
+
+        action(context);
+
+        "End CORS".Dump();
+    }
+}
---
+++ src/Javid Hassani/PipelineDesignPattern/PipelineDesignPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PipelineDesignPattern;
+
+Console.WriteLine("Hi, Please Enter your Ip");
+
+string Ip = Console.ReadLine()!;
+while (true)
+{
+    if (string.IsNullOrEmpty(Ip))
+    {
+        Console.WriteLine("Please Enter your Ip");
+        Ip = Console.ReadLine()!;
+    }
+    else
+        break;
+}
+
+Context httpContext = new() { Ip = Ip };
+var pipe = new Pipe();
+var userService = new UserService();
+
+pipe.ExceptionHandling(httpContext,
+    (context) => pipe.Authurization(httpContext,(context) => pipe.Cors(httpContext, userService.GetUserData)));
+
+Console.ReadKey();
+
