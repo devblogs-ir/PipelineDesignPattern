7536605460126ca7d03c4ef7349a45c582e9afac
hamidreza.rahmani9979@outlook.com <hamidreza.rahmani9979@outlook.com>
2023-12-19 22:30:35 +0330

    Add Simple pipeline design pattern project

---
+++ src/hamidreza rahmani/PipelineDesignPattern/Framework.cs
@@ -0,0 +1,28 @@
+﻿using Dumpify;
+using System;
+
+namespace PipelineDesignPattern;
+
+public class Framework
+{
+    public void Authentication(Action<HttpContext> func, HttpContext httpContent)
+    {
+        if (httpContent.Ip is "192.168.0.130")
+        {
+            throw new CountryBlockedException("you are from iran");
+        }
+        func(httpContent);
+    }
+
+    public void ExceptionHandling(Action<HttpContext> func, HttpContext httpContent)
+    {
+        try
+        {
+            func(httpContent);
+        }
+        catch (CountryBlockedException e)
+        {
+            "catched".Dump(e.Message);
+        }
+    }
+}
