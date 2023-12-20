75457a00580fd3151e97543c90eed8060761ec6f
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2023-12-20 13:43:36 +0330

    Simple pipeline design pattern

---
+++ src/ArezooKamane/PipelineDesignPattern/PipelineDesignPattern/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dumpify;
+
+namespace PipelineDesignPattern
+{
+    public class ProductController
+    {
+        public void GetAllProducts(HttpContext httpContext)
+        {
+            "all products".Dump();
+        }
+    }
+}
