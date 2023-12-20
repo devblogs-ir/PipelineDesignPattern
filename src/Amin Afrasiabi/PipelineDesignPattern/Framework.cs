bd46e22011831b11af12dd39742176b845fc7446
aminafra <mohamadamin.afrasiabi@gmail.com>
2023-12-20 21:11:43 +0330

    add PipelineDesignPattern Project

---
+++ src/Amin Afrasiabi/PipelineDesignPattern/Framework.cs
@@ -0,0 +1,28 @@
+﻿using Dumpify;
+
+namespace PipelineDesignPattern;
+public class Framework
+{
+    public void Authentication(HttpContext httpContext, Action<HttpContext> action)
+    {
+		"Start Authentication...".Dump();
+		if (httpContext.Country is CountryIPPAddress.Iran)
+		{
+			throw new Exception(message: "Iranian IPs are blocked");
+		}
+		action(httpContext);
+    }
+
+    public void ExceptionHandling(HttpContext httpContext, Action<HttpContext> action)
+    {
+		try
+		{
+			action(httpContext);
+			"Done".Dump();
+		}
+		catch (Exception ex)
+		{
+			ex.Message.Dump();
+		}
+    }
+}
---
+++ src/Amin Afrasiabi/PipelineDesignPattern/HttpContext.cs
@@ -0,0 +1,13 @@
+﻿namespace PipelineDesignPattern;
+public class HttpContext
+{
+    public CountryIPPAddress Country => Enum.TryParse(IP[^2..], out CountryIPPAddress country) ? country : 0;
+    public required string IP { get; set; }
+}
+public enum CountryIPPAddress
+{
+    UnKnown = 0,
+    Iran = 10,
+    China = 20,
+    USA = 30,
+}
