34af4d48a7b20384364e255b0688f3399dda5f47
Mohammad Mahdi Mollaeian <mahdi.mollaeian@gmail.com>
2023-12-20 20:27:23 +0330

    feat:complete App

---
+++ src/mollaeian/PipeLineApp/FilteringController.cs
@@ -0,0 +1,11 @@
+﻿using Dumpify;
+
+namespace PipeLineApp;
+
+public class FilteringController
+{
+    public void GetIp(HttpContext httpContext)
+    {
+        $"ip address is {httpContext.Ip}".Dump("FilteringController.GetIp");
+    }
+}
---
+++ src/mollaeian/PipeLineApp/Framework.cs
@@ -0,0 +1,26 @@
+﻿using Dumpify;
+
+namespace PipeLineApp;
+
+public class Framework
+{
+    public static void Authentication(HttpContext context, Action<HttpContext> action)
+    {
+        context.Ip.Dump("Starting Auth");
+        if (context.Ip == "192.168.1.1") throw new Exception("you are from iran");
+        action(context);
+        "End Auth".Dump();
+    }
+
+    public static void ExceptionHandling(HttpContext context, Action<HttpContext> action)
+    {
+        try
+        {
+            action(context);
+        }
+        catch (Exception e)
+        {
+            "Exception Catch".Dump(e.Message);
+        }
+    }
+}
