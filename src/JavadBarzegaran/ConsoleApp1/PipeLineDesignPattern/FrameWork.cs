2c32089da8d7a0e08d4a817a0331d1633f079b02
DARYASALAR JAVAD <DARYASALAR JAVAD@gmail.com>
2023-12-20 17:20:53 +0330

    Add Files From Javad

---
+++ src/JavadBarzegaran/ConsoleApp1/PipeLineDesignPattern/FrameWork.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleDump;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipeLineDesignPattern
+{
+    public class FrameWork
+    {
+        public void ExceptionHandling(HttpContext httpContext, Action<HttpContext> action)
+        {
+            try
+            {
+                action(httpContext);
+                "Welcome,You Are From CANADA".Dump();
+            }
+            catch(Exception ex)
+            {
+                "Eror,You Are From Iran".Dump();
+            }    
+        }
+
+        public void Authentication(HttpContext httpContext)
+        {
+            if (httpContext.UserIp.Equals("IRAN"))
+            {
+                throw new Exception();
+            }
+        }
+
+
+    }
+}
---
+++ src/JavadBarzegaran/ConsoleApp1/PipeLineDesignPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using PipeLineDesignPattern;
+
+var framework = new FrameWork();
+
+var httpContext1 = new HttpContext() { UserIp = "IRAN" };
+
+var httpContext2 = new HttpContext() { UserIp = "CANADA" };
+
+framework.ExceptionHandling(httpContext1, framework.Authentication);
+
+framework.ExceptionHandling(httpContext2, framework.Authentication);
+
+
+
+
+
