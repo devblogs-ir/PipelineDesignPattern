8c3db8901f0456b876d9aaf8579843c27857e90a
Mr.Salahinia <salahimahdi938@gmail.com>
2023-12-21 10:23:44 +0330

    UseExpressionCollectionInModelClass

--- src/Mahdi Salahinia/PipelineDesignPatternExp/CountryIp.cs
+++ src/Mahdi Salahinia/PipelineDesignPatternExp/CountryIp.cs
@@ -8,25 +8,7 @@ namespace PipelineDesignPatternExp
 {
     public class CountryIp
     {
-        public List<string> IranIps { get; set; }
-        public List<string> AmericaIps { get; set; }
-
-        public CountryIp()
-        {
-            IranIps = new List<string>()
-            {
-                "192.168.22.7",
-                "192.168.22.8",
-                "192.168.22.9",
-                "192.168.22.4"
-            };
-
-            AmericaIps = new List<string>()
-            {
-                "1.32.232.0",
-                "2.16.55.0",
-                "2.16.203.1",
-            };
-        }
+        public List<string> IranIps { get; set; } = ["192.168.22.7", "192.168.22.8", "192.168.22.9"];
+        public List<string> AmericaIps { get; set; } = ["1.32.232.0", "2.16.55.0", "2.16.203.1"];
     }
 }
