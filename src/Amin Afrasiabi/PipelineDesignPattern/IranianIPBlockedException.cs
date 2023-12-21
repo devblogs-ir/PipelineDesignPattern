7708e97f3535595addc96607179c20781b975471
aminafra <mohamadamin.afrasiabi@gmail.com>
2023-12-21 20:54:05 +0330

    Create Custom Exeption

---
+++ src/Amin Afrasiabi/PipelineDesignPattern/IranianIPBlockedException.cs
@@ -0,0 +1,7 @@
+﻿namespace PipelineDesignPattern;
+public class IranianIPBlockedException : Exception
+{
+    public IranianIPBlockedException() { }
+    public IranianIPBlockedException(string message) : base(message) { }
+    public IranianIPBlockedException(string message, Exception inner) : base(message, inner) { }
+}
