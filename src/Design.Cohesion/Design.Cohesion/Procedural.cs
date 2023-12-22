8f71ef72657abcd5f0abfa7eb8790285944f9c1c
sara <saragholampour971@yahoo.com>
2023-12-22 20:44:57 +0330

    add cohesion.procedural

---
+++ src/Design.Cohesion/Design.Cohesion/Procedural.cs
@@ -0,0 +1,33 @@
+﻿namespace Design.Cohesion;
+
+public class Procedural
+{
+    public List<int> nums { get; set; }
+
+    public Procedural(List<int> numbers) => nums = numbers;
+
+    public int Sum(List<int> nums) => nums.Sum();
+
+    public int Average(List<int> nums) => Sum(nums) / nums.Count;
+    
+  
+    
+    public int Variance(List<int> nums,int average)
+    {
+        int result = 0;
+        foreach (var x in nums)
+        {
+            result+= (x - average) ^ 2;
+        }
+
+        result = result / (nums.Count - 1);
+
+        return result;
+
+    }
+    public double StandardDeviation(List<int> nums)
+    {
+        int average = Average(nums);
+        return Math.Sqrt(Variance(nums,average));
+    }
+}
