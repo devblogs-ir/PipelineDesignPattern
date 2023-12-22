180de5a915510d0e68eff43fe092af619e2687cb
sara <saragholampour971@yahoo.com>
2023-12-22 20:16:09 +0330

    add cohesion.communication

---
+++ src/Design.Cohesion/Design.Cohesion/Communication.cs
@@ -0,0 +1,13 @@
+﻿namespace Design.Cohesion;
+
+public class Communication
+{
+    public int EnergyPercentage { get; set; }
+
+
+    public void Sleep()=> EnergyPercentage += 4;
+    
+
+    public void Work() => EnergyPercentage -= 10;
+
+}
