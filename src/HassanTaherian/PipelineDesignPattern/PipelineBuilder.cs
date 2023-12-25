9fc6eda5f22d2d526ec39cdf2fcaa8560c748333
Hassan Taherian <hassantsf.dev@gmail.com>
2023-12-25 18:41:07 +0330

    refactor: Define builder for hiding complexity of creating and chaining Handlers

---
+++ src/HassanTaherian/PipelineDesignPattern/PipelineBuilder.cs
@@ -0,0 +1,35 @@
+﻿using PipelineDesignPattern.Handlers;
+
+namespace PipelineDesignPattern;
+public class PipelineBuilder
+{
+    private BaseHandler? entryPoint { get; set; }
+    private BaseHandler? lastHandler { get; set; }
+
+    public PipelineBuilder AddHandler(BaseHandler baseHandler)
+    {
+        if (!IsThereHandlerInPipeline())
+        {
+            entryPoint = baseHandler;
+            lastHandler = baseHandler;
+        }
+        else
+        {
+            lastHandler.Next = baseHandler.Handle;
+            lastHandler = baseHandler;
+        }
+        return this;
+    }
+
+    private bool IsThereHandlerInPipeline()
+    {
+        return lastHandler is not null;
+    }
+
+
+    public void Run(HttpContext httpContext)
+    {
+        entryPoint?.Handle(httpContext);
+    }
+
+}
