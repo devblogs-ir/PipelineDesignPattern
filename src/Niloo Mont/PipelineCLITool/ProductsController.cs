2f3543f12de1bc75dfb08945550eff31c831a176
Niloo Mont <niloo.mon@gmail.com>
2023-12-25 20:39:09 +0330

    Completed a workinig pipeline

---
+++ src/Niloo Mont/PipelineCLITool/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Dumpify;
+
+namespace PipelineCLITool;
+public class ProductsController
+{
+    private readonly HttpContext _httpContext;
+
+    public ProductsController(HttpContext httpContext)
+    {
+        _httpContext = httpContext;
+    }
+    // Products/GetAllProducts
+    public void GetAllProducts()
+    {
+        "List of all products.".Dump();
+    }
+    List<User> users = new List<User>()
+    {
+        new User() { Id = 1, Name = "User1" },
+        new User() { Id = 2, Name = "User2" },
+        new User() { Id = 3, Name = "User3" },
+        new User() { Id = 4, Name = "User4" },
+        new User() { Id = 3, Name = "User5" },
+    };
+    public string GetUserById(int id)
+    {
+        if (!users.Any(p => p.Id == id))
+            return "No user was found!".Dump();
+        return users.SingleOrDefault(p => p.Id == id)
+            .Name.Dump();
+    }
+}
---
+++ src/Niloo Mont/PipelineCLITool/Program.cs
@@ -0,0 +1,33 @@
+﻿using PipelineCLITool;
+
+Framework framework = new();
+
+HttpContext GetAllProductsRequest = new()
+{
+    IP = "Iran",
+    Url = "localhost:7777/Products/GetAllProducts"
+};
+HttpContext GetAllProductsRequest2 = new()
+{
+    IP = "USA",
+    Url = "localhost:7777/Products/GetUserById/2"
+};
+HttpContext GetAllProductsRequest3 = new()
+{
+    IP = "USA",
+    Url = "localhost:7777/Products/GetAllProducts"
+};
+
+framework.AuthenticationHandling(GetAllProductsRequest,
+    (context) => framework.ExceptionHandling((context),
+    (context) => framework.EndpointHandling(context, null!)));
+
+framework.AuthenticationHandling(GetAllProductsRequest2,
+    (context) => framework.ExceptionHandling((context),
+    (context) => framework.EndpointHandling(context, null!)));
+
+framework.AuthenticationHandling(GetAllProductsRequest3,
+    (context) => framework.ExceptionHandling((context),
+    (context) => framework.EndpointHandling(context, null!)));
+
+
