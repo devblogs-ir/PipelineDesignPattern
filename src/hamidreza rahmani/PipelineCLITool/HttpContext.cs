8c6a3006893dceeb293ef22d70428ef87b53ccff
hamidreza <hamidreza.rahmani99@gmail.com>
2023-12-26 14:12:37 -0500

    Add BuilderPattern

---
+++ src/hamidreza rahmani/PipelineCLITool/HttpContext.cs
@@ -0,0 +1,10 @@
+using System.Runtime.InteropServices;
+
+namespace PipelineDesignPattern;
+
+public class HttpContext
+{
+    public required string IP { get; set; }
+    // Products/GetAll
+    public string Url { get; set; }
+}
---
+++ src/hamidreza rahmani/PipelineCLITool/Program.cs
@@ -0,0 +1,28 @@
+﻿using PipelineDesignPattern;
+using System.Reflection;
+
+Framework framework = new();
+
+HttpContext requestUser1 = new() {
+    IP = "123.185.20.177" ,
+    Url = "localhost:4545/Products/GetUserById/0"
+};
+
+HttpContext requestUser2 = new()
+{
+    IP = "123.185.20.177",
+    Url = "localhost:4545/Products/GetUserById/1"
+};
+  
+var pipeline = new PipelineBuilder()
+                        .AddPipe(typeof(ExceptionHandlingPipe))
+                        .AddPipe(typeof(AuthenticationPipe))
+                        .AddPipe(typeof(EndPointPipe))
+                        .Build();
+
+pipeline.Handle(requestUser1);
+pipeline.Handle(requestUser2);
+pipeline.Handle(requestUser2);
+pipeline.Handle(requestUser1);
+pipeline.Handle(requestUser2);
+
