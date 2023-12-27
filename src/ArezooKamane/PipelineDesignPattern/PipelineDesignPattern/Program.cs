4e8f3c695fd522b6aae7530f0e91f6feeef00609
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2023-12-27 15:12:42 +0330

    create cli tool

--- src/ArezooKamane/PipelineDesignPattern/PipelineDesignPattern/Program.cs
+++ src/ArezooKamane/PipelineDesignPattern/PipelineDesignPattern/Program.cs
@@ -1,29 +1,56 @@
-﻿namespace PipelineDesignPattern;
+﻿using CommandLine;
+
+namespace PipelineDesignPattern
+{
     internal class Program
     {
-        static void Main(string[] args)
+        private static string _ip;
+        private static string _url;
+
+    static void Main(string[] args)
         {
-            //HttpContext context = new HttpContext() { IP = "178.252.190.1" };
-            HttpContext context1 = new HttpContext()
-            {
-                IP = "8.8.8.8",
-                Url = "localhost:8080/Products/GetAllProducts"
-
-            };
+        //iran IP = "178.252.190.1
+        //IP = "8.8.8.8",
+        //Url = "localhost:8080/Products/GetAllProducts"
 
 
-            var pipeline = new PipelineBuilder()
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(RunOptions)
+            .WithNotParsed(HandleParseError);
+
+
+
+        var pipeline = new PipelineBuilder()
                 .UsePipe(typeof(ExceptionHandlingPipe))
                 .UsePipe(typeof(AuthenticationPipe))
                 .UsePipe(typeof(EndPointPipe))
                 .Build();
 
 
-            pipeline.Invoke(context1);
-
-
-
+            pipeline.Invoke(new HttpContext(){IP = _ip, Url = _url});
 
     }
-}
 
+        static void RunOptions(Options opts)
+        {
+            _ip = opts.IP;
+            _url = opts.Url;
+        }
+
+        static void HandleParseError(IEnumerable<Error> errors)
+        {
+            Console.WriteLine("Failed to parse command line options."); 
+        }
+
+}
+class Options
+{
+    [Option('i', "ip", Required = true, HelpText ="IP")]
+    public string IP { get; set; }
+
+
+    [Option('u', "url", Required = true, HelpText = "URL")]
+    public string Url { get; set; }
+
+}
+}
